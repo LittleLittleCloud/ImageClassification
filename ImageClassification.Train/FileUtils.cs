f8c04a7cc327adc0c45a8748e7df66621c2c2c60
LittleLittleCloud <bigmiao.zhang@gmail.com>
2020-11-02 10:45:25 -0800

    init

---
+++ ImageClassification.Train/FileUtils.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using ImageClassification.DataModels;
+
+namespace ImageClassification
+{
+    public class FileUtils
+    {
+        public static IEnumerable<(string imagePath, string label)> LoadImagesFromDirectory(
+            string folder,
+            bool useFolderNameasLabel)
+        {
+            var imagesPath = Directory
+                .GetFiles(folder, "*", searchOption: SearchOption.AllDirectories)
+                .Where(x => Path.GetExtension(x) == ".jpg" || Path.GetExtension(x) == ".png");
+
+            return useFolderNameasLabel
+                ? imagesPath.Select(imagePath => (imagePath, Directory.GetParent(imagePath).Name))
+                : imagesPath.Select(imagePath =>
+                {
+                    var label = Path.GetFileName(imagePath);
+                    for (var index = 0; index < label.Length; index++)
+                    {
+                        if (!char.IsLetter(label[index]))
+                        {
+                            label = label.Substring(0, index);
+                            break;
+                        }
+                    }
+                    return (imagePath, label);
+                });
+        }
+
+        public static string GetAbsolutePath(Assembly assembly, string relativePath)
+        {
+            var assemblyFolderPath = new FileInfo(assembly.Location).Directory.FullName;
+
+            return Path.Combine(assemblyFolderPath, relativePath);
+        }
+    }
+}
